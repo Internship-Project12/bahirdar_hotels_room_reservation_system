e779e923111b1e110f2b96c16eecee34b2a939dd
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-05 21:08:38 +0300

    feat: AppLayout component added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,15 +5,18 @@ import {
   Navigate,
 } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import AppLayout from "./components/AppLayout";
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        {/* <Route path='/login' element={<LoginPage />} />
+        <Route element={<AppLayout />}>
+          <Route path="/" element={<HomePage />} />
+          {/* <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<RegisterPage />} />
-        <Route path='/dashboard' element={<DashboardPage />} /> */}
-        <Route path="/*" element={<Navigate to="/" />} />
+      <Route path='/dashboard' element={<DashboardPage />} /> */}
+          <Route path="/*" element={<Navigate to="/" />} />
+        </Route>
       </Routes>
     </Router>
   );
