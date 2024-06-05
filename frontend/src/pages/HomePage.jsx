d44f70b9a505697cba1b6fc61e4d1f6b05609ae2
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-05 21:55:46 +0300

    feat: hero section | home page finished

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
-    <main className="flex min-h-full flex-col bg-slate-200">
-      <nav className="flex justify-between bg-blue-800 px-10 py-6">
-        <span className="text-3xl font-bold tracking-tight text-gray-100">
+    <main className="mb-2 flex min-h-full flex-col gap-4 bg-slate-200">
+      <nav className="flex justify-between gap-10 bg-blue-800 px-10 py-6">
+        <span className="text-3xl font-extrabold tracking-tighter text-gray-100">
           BDHotels.com
         </span>
         <ul className="flex justify-between gap-4">
@@ -16,22 +16,17 @@ function HomePage() {
               Sign In
             </Link>
           </li>
-          <li>
-            <Link
-              to="sign-up"
-              className="flex items-center rounded bg-slate-200 px-3 py-2 font-bold text-blue-800 transition duration-300"
-            >
-              Sign Up
-            </Link>
-          </li>
         </ul>
       </nav>
-      <section className="mt-24 flex justify-around gap-4 px-8">
-        <div className="">
+      <section className="flex flex-col items-center gap-4 px-8 md:flex-row">
+        <div className="flex-1 p-4 text-2xl font-light tracking-wider">
           <p className="">
-            <h1 className="">
-              Welcome to <span>BDHotels.com</span>
-            </h1>
+            <span className="">
+              Welcome to{" "}
+              <span className="text-3xl font-extrabold tracking-tighter text-blue-800">
+                BDHotels.com
+              </span>
+            </span>{" "}
             where every stay is a memorable experience. Escape to luxury and
             convenience - book your dream hotel stay effortlessly.
           </p>
@@ -41,11 +36,15 @@ function HomePage() {
             intuitive hotel booking platform .
           </p>
         </div>
-        <div className="">
-          <img src="./hotel-images/img-1-hero.jpg" alt="hero image" />
+        <div className="flex-1 overflow-hidden rounded-lg p-4 md:max-h-[95%]">
+          <img
+            src="./hotel-images/img-1-hero.jpg"
+            alt="Hero Image"
+            className="h-full w-full object-cover"
+          />
         </div>
       </section>
-      <div className="mt-8 flex justify-center">
+      <div className="flex justify-center">
         <button className="rounded bg-blue-800 px-6 py-4 font-bold uppercase text-slate-200 shadow-xl">
           Explore all the Hotels in the the town
         </button>
