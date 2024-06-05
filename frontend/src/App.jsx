ce197030f08c7280455a11bbe69122f7eac14761
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-05 20:23:33 +0300

    static: add sample images

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,22 @@
-export default function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/dashboard' element={<DashboardPage />} /> */}
+        <Route path="/*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
 }
+
+export default App;
