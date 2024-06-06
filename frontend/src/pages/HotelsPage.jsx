056f48e38aeea39db0cf20b0cacd980b4494e566
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-06 21:38:34 +0300

    feat: hotels page updated | make each hotels card

--- frontend/src/pages/HotelsPage.jsx
+++ frontend/src/pages/HotelsPage.jsx
@@ -1,153 +1,103 @@
 import { FaStar, FaRegStar } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 function HotelsPage() {
   return (
     <div className="relative">
-      <div className="justify-cente sticky top-4 flex">
+      <div className="sticky top-4 flex justify-center">
         {/* filter/sort */}
         <div className="h-[100vh] min-w-[300px] border-r-2 border-r-blue-300 bg-blue-100">
           filter/sort
         </div>
         {/* hotels list */}
-        <section className="h-screen max-w-[1024px] flex-auto overflow-scroll bg-gray-200">
+        <section className="h-screen max-w-[1024px] flex-1 overflow-scroll bg-gray-200">
           {/* hotel cards */}
-          <div className="m-4 flex justify-between overflow-hidden rounded">
-            <div className="flex gap-3">
-              <div className="flex-1 overflow-hidden rounded shadow-xl">
-                <img
-                  src="/hotel-images/img-4.jpg"
-                  alt=""
-                  className="h-full max-w-[300px] object-cover"
-                />
+          {[1, 2, 3, 4, 5, 6].map((val) => (
+            <div
+              key={val}
+              className="m-4 flex justify-between overflow-hidden rounded border-2 border-blue-300 p-3"
+            >
+              <div>
+                <div className="flex gap-3">
+                  <div className="h-[300px] min-w-[300px] flex-1 overflow-hidden rounded shadow-xl">
+                    <img
+                      src="/hotel-images/img-4.jpg"
+                      alt=""
+                      className="h-full max-w-[300px] object-cover"
+                    />
+                  </div>
+                  {/* hotel name */}
+                  <div className="flex flex-col justify-around">
+                    <div className="flex flex-col gap-2">
+                      <Link
+                        to={`/hotels/${1}`}
+                        className="text-2xl font-bold text-blue-900 underline"
+                      >
+                        Azwa International Hotel
+                      </Link>
+                      <span className="text-sm text-slate-500">
+                        (7 Star Hotel In Bahirdar)
+                      </span>
+                    </div>
+                    <div className="flex flex-col justify-center gap-3">
+                      <span>Standard Rooms</span>
+                      <div className="">
+                        <p>
+                          <span>3</span> Double bed
+                        </p>
+                        <p>
+                          <span>2</span> Single bed
+                        </p>
+                      </div>
+                    </div>
+                    <p>free Cancelation && free Breakfast</p>
+                  </div>
+                </div>
+                <p className="mt-2 text-sm">
+                  <span>total of 5 floors(G + 5) </span>{" "}
+                </p>
               </div>
-              {/* hotel name */}
-              <div className="flex-1">
-                <h2>Azwa International Hotel</h2>
 
-                <span>5 Star</span>
+              {/* summary */}
+              <div className="flex max-w-[200px] flex-col items-end justify-evenly">
+                <div className="flex flex-col items-end gap-1">
+                  <div className="flex items-center gap-2">
+                    <span className="flex gap-1">
+                      <FaStar color="#fcc419" fill="#fcc419" size={"20px"} />
+                      <FaStar color="#fcc419" fill="#fcc419" size={"20px"} />
+                      <FaStar color="#fcc419" fill="#fcc419" size={"20px"} />
+                      <FaStar color="#fcc419" fill="#fcc419" size={"20px"} />
+                      <FaRegStar color="#fcc419" fill="#fcc419" size={"20px"} />
+                    </span>
+                    <span className="font-bold tracking-tighter text-yellow-400">
+                      4.0
+                    </span>
+                  </div>
+                  <h3 className="text-gray-5 border-b-2 border-blue-800 font-light tracking-tight">
+                    <span className="font-bold text-blue-800">34</span> reviews
+                  </h3>
+                </div>
+                <div className="flex flex-col items-end">
+                  <p className="text-lg font-bold text-slate-600">3 Nights</p>
+                  <p className="text-xs font-bold text-slate-600">
+                    (2 Adults, 1 Children)
+                  </p>
+                  <p className="text-sm font-bold text-slate-600">
+                    Price:{" "}
+                    <span className="text-lg font-bold tracking-tighter text-blue-800">
+                      $230
+                    </span>
+                  </p>
+                </div>
+                <Link
+                  to={`/booking/${1}`}
+                  className="rounded bg-blue-500 p-2 text-white"
+                >
+                  Book Now
+                </Link>
               </div>
             </div>
-
-            {/* summary */}
-            <div className="flex min-w-[200px] flex-1 flex-col items-end bg-slate-400">
-              <div className="flex items-center gap-2">
-                <span className="flex gap-1">
-                  <FaStar color="#fcc419" fill="#fcc419" size={"20px"} />
-                  <FaStar color="#fcc419" fill="#fcc419" size={"20px"} />
-                  <FaStar color="#fcc419" fill="#fcc419" size={"20px"} />
-                  <FaStar color="#fcc419" fill="#fcc419" size={"20px"} />
-                  <FaRegStar color="#fcc419" fill="#fcc419" size={"20px"} />
-                </span>
-                <span className="font-bold tracking-tighter text-yellow-400">
-                  4.0
-                </span>
-              </div>
-              <h3>34 reviews</h3>
-            </div>
-          </div>
-
-          <div className="m-4 flex justify-between overflow-hidden rounded">
-            <div className="flex gap-3">
-              <div className="flex-1 overflow-hidden rounded shadow-xl">
-                <img
-                  src="/hotel-images/img-4.jpg"
-                  alt=""
-                  className="h-full max-w-[300px] object-cover"
-                />
-              </div>
-              {/* hotel name */}
-              <div className="flex-1">
-                <h2>Azwa International Hotel</h2>
-
-                <span>5 Star</span>
-              </div>
-            </div>
-
-            {/* summary */}
-            <div className="flex min-w-[200px] flex-1 flex-col items-end bg-slate-400">
-              <div className="flex items-center gap-2">
-                <span className="flex gap-1">
-                  <FaStar color="#fcc419" fill="#fcc419" size={"20px"} />
-                  <FaStar color="#fcc419" fill="#fcc419" size={"20px"} />
-                  <FaStar color="#fcc419" fill="#fcc419" size={"20px"} />
-                  <FaStar color="#fcc419" fill="#fcc419" size={"20px"} />
-                  <FaRegStar color="#fcc419" fill="#fcc419" size={"20px"} />
-                </span>
-                <span className="font-bold tracking-tighter text-yellow-400">
-                  4.0
-                </span>
-              </div>
-              <h3>34 reviews</h3>
-            </div>
-          </div>
-          <div className="m-4 flex justify-between overflow-hidden rounded">
-            <div className="flex gap-3">
-              <div className="flex-1 overflow-hidden rounded shadow-xl">
-                <img
-                  src="/hotel-images/img-4.jpg"
-                  alt=""
-                  className="h-full max-w-[300px] object-cover"
-                />
-              </div>
-              {/* hotel name */}
-              <div className="flex-1">
-                <h2>Azwa International Hotel</h2>
-
-                <span>5 Star</span>
-              </div>
-            </div>
-
-            {/* summary */}
-            <div className="flex min-w-[200px] flex-1 flex-col items-end bg-slate-400">
-              <div className="flex items-center gap-2">
-                <span className="flex gap-1">
-                  <FaStar color="#fcc419" fill="#fcc419" size={"20px"} />
-                  <FaStar color="#fcc419" fill="#fcc419" size={"20px"} />
-                  <FaStar color="#fcc419" fill="#fcc419" size={"20px"} />
-                  <FaStar color="#fcc419" fill="#fcc419" size={"20px"} />
-                  <FaRegStar color="#fcc419" fill="#fcc419" size={"20px"} />
-                </span>
-                <span className="font-bold tracking-tighter text-yellow-400">
-                  4.0
-                </span>
-              </div>
-              <h3>34 reviews</h3>
-            </div>
-          </div>
-          <div className="m-4 flex justify-between overflow-hidden rounded">
-            <div className="flex gap-3">
-              <div className="flex-1 overflow-hidden rounded shadow-xl">
-                <img
-                  src="/hotel-images/img-4.jpg"
-                  alt=""
-                  className="h-full max-w-[300px] object-cover"
-                />
-              </div>
-              {/* hotel name */}
-              <div className="flex-1">
-                <h2>Azwa International Hotel</h2>
-
-                <span>5 Star</span>
-              </div>
-            </div>
-
-            {/* summary */}
-            <div className="flex min-w-[200px] flex-1 flex-col items-end bg-slate-400">
-              <div className="flex items-center gap-2">
-                <span className="flex gap-1">
-                  <FaStar color="#fcc419" fill="#fcc419" size={"20px"} />
-                  <FaStar color="#fcc419" fill="#fcc419" size={"20px"} />
-                  <FaStar color="#fcc419" fill="#fcc419" size={"20px"} />
-                  <FaStar color="#fcc419" fill="#fcc419" size={"20px"} />
-                  <FaRegStar color="#fcc419" fill="#fcc419" size={"20px"} />
-                </span>
-                <span className="font-bold tracking-tighter text-yellow-400">
-                  4.0
-                </span>
-              </div>
-              <h3>34 reviews</h3>
-            </div>
-          </div>
+          ))}
         </section>
       </div>
     </div>
