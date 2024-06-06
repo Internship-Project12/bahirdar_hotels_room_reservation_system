801e0f259d2092852e5bfe002a7cdc7f21fa18d9
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-06 11:23:00 +0300

    feat: change explore more btn to Link

--- frontend/src/components/homepage/ExploreMoreBtn.jsx
+++ frontend/src/components/homepage/ExploreMoreBtn.jsx
@@ -1,11 +1,16 @@
+import { Link } from "react-router-dom";
+
 function ExploreMoreBtn() {
   return (
-    <div className="flex justify-center">
-      <button className="rounded bg-blue-800 px-6 py-4 font-bold uppercase text-slate-200 shadow-xl">
+    <div className="flex min-w-[50vh] justify-center">
+      <Link
+        to="/hotels"
+        className="rounded bg-blue-800 px-6 py-4 text-sm font-bold uppercase text-slate-200 shadow-2xl"
+      >
         Explore all the Hotels in the the town
-      </button>
+      </Link>
     </div>
   );
 }
 
-export default ExploreMoreBtn
+export default ExploreMoreBtn;
