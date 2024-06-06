00fb3cb4649cb1c08fc9465840e7d676039a7d1d
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-06 14:55:00 +0300

    feat: update nav links | add a hotels link

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <header className="sticky top-0 flex min-w-full justify-between bg-blue-800 px-24 py-6 opacity-85">
+    <header className="top-0 flex min-w-full justify-between bg-blue-800 px-24 py-6 opacity-85">
       <Link
         to="/"
         className="text-3xl font-extrabold tracking-tighter text-gray-100"
@@ -11,6 +11,22 @@ function Header() {
       </Link>
       <nav>
         <ul className="flex justify-between gap-4">
+          <li>
+            <Link
+              to="/hotels"
+              className="flex items-center rounded px-3 py-2 font-bold text-blue-100 underline transition duration-300"
+            >
+              hotels
+            </Link>
+          </li>
+          {/* <li>
+            <a
+              href="#services-section"
+              className="flex items-center rounded px-3 py-2 font-bold text-blue-100 underline transition duration-300"
+            >
+              services
+            </a>
+          </li>{" "} */}
           <li>
             <Link
               to="/sign-in"
