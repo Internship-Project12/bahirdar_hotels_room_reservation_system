88e76ac3d9c262e6b8413be54dce56e5b0a7e506
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-10 14:11:58 +0300

    feat: manage hotel form | handleSubmit

--- frontend/src/components/forms/manageHotelForm/ManageHotelForm.jsx
+++ frontend/src/components/forms/manageHotelForm/ManageHotelForm.jsx
@@ -1,25 +1,31 @@
+/* eslint-disable react/prop-types */
 import { FormProvider, useForm } from "react-hook-form";
 
 import DetailSection from "./DetailSection";
 import ImageSection from "./ImageSection";
 import SummarySection from "./SummarySection";
 
-function ManageHotelForm() {
+function ManageHotelForm({ isPending, onSubmit }) {
   const formMethods = useForm();
-  const {handleSubmit} = formMethods;
+  const { handleSubmit } = formMethods;
 
-  const  onSubmitHandler = handleSubmit(data => {
-    console.log(data)
-  })
+  const onSubmitHandler = handleSubmit((data) => {
+    onSubmit(data);
+  });
+
   return (
     <FormProvider {...formMethods}>
-      <form onSubmit={onSubmitHandler} className="m-auto flex max-w-[85%] flex-col gap-8 rounded bg-slate-100 p-10 shadow-lg">
+      <form
+        onSubmit={onSubmitHandler}
+        className="m-auto flex max-w-[85%] flex-col gap-8 rounded bg-slate-100 p-10 shadow-lg"
+      >
         <DetailSection />
         <SummarySection />
         <ImageSection />
         <button
           type="submit"
-          className="w-full rounded bg-blue-800 px-3 py-2 text-white"
+          className="w-full rounded bg-blue-800 px-3 py-2 text-white disabled:cursor-not-allowed disabled:bg-gray-400"
+          disabled={isPending}
         >
           Add Hotel
         </button>
