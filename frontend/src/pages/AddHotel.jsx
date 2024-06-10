8523415c2d817acefef206b140e5dfd409a3138c
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-10 14:10:25 +0300

    feat: useMutation to add a hotel | addHotel page

--- frontend/src/pages/AddHotel.jsx
+++ frontend/src/pages/AddHotel.jsx
@@ -1,9 +1,24 @@
+import { useMutation } from "@tanstack/react-query";
 import ManageHotelForm from "../components/forms/manageHotelForm/ManageHotelForm";
+import apiHotels from "../services/api-hotels";
+import toast from "react-hot-toast";
 
 function AddHotel() {
-  return (
-      <ManageHotelForm />
-  );
+  const { mutate, isPending } = useMutation({
+    mutationFn: apiHotels.addHotel,
+    onSuccess: () => {
+      toast.success("Hotel added successfully");
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
+  const handleSubmit = (hotel) => {
+    mutate(hotel);
+  };
+
+  return <ManageHotelForm onSubmit={handleSubmit} isPending={isPending} />;
 }
 
 export default AddHotel;
