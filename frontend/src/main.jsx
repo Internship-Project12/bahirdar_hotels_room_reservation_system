089d96a35e86169efeaf7e820de46690b26bdaf6
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-10 13:35:26 +0300

    feat: setup react query | devtools

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App.jsx";
 import "./index.css";
-import customFetch from "./utils/customFetch.js";
 
-const res = await customFetch.get("/api/v1/test");
-console.log(res);
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
