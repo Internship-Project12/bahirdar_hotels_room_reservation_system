7822c34f2d54cb34fdb03d90c2552e2197d5fbe3
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-10 14:09:38 +0300

    feat: add a toaster on main.jsx | frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 import App from "./App.jsx";
 import "./index.css";
+// import customFetch from "./utils/customFetch.js";
+
+// const res = await customFetch.get("/api/v1/hotels");
+// console.log(res.data);
 
 const queryClient = new QueryClient();
 
@@ -11,6 +16,26 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 5000,
+          },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "#f8fafc",
+            color: "#0f172a",
+          },
+        }}
+      />
       <App />
     </QueryClientProvider>
   </React.StrictMode>,
