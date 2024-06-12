39d50c51e87d5f81ca8a1d713b55fc1099a2d905
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-12 10:10:48 +0300

    feat: auth controller | add login in fn

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -17,3 +17,29 @@ export const signup = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+export const login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1) Check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password!', 400));
+  }
+
+  // 2) Check if user exists && password is correct
+  const user = await User.findOne({ email }).select('+password');
+
+  const isCorrectPass = await user?.isCorrectPassword(password, user.password);
+
+  if (!user || !isCorrectPass) {
+    return next(new AppError(' Incorrect email or password', 401));
+  }
+
+  // 3) If everything ok, send token to client
+  const token = createJWT({ id: user._id });
+
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+});
