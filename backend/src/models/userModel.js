d095fe2e336ce98a3f091b4168417949914b6a35
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-12 10:14:38 +0300

    feat: create a user model

---
+++ backend/src/models/userModel.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+import validator from 'validator'; //TODO: INSTALL
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, 'Please tell us your name!'],
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: [true, 'Please tell us your name!'],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: [8, 'Password must have at least 8 characters'],
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on CREATE and SAVE!!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!',
+    },
+  },
+  passwordChangedAt: Date,
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
