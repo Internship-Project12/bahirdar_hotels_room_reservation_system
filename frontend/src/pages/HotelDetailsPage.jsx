43e62267f7be699d70d7083e2cc7eff67f431a65
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-12 14:09:03 +0300

    feat: hotel detail page | fetch the data and display

--- frontend/src/pages/HotelDetailsPage.jsx
+++ frontend/src/pages/HotelDetailsPage.jsx
@@ -1,27 +1,41 @@
 import { useQuery } from "@tanstack/react-query";
 import apiHotels from "../services/api-hotels";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { toast } from "react-hot-toast";
 
 function HotelDetailsPage() {
   const { id } = useParams();
-  console.log(id);
+  const navigate = useNavigate();
 
-  const { data: res, isLoading } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: ["hotel", id],
     queryFn: () => apiHotels.getHotel(id),
   });
+
+  console.log(data);
 
   if (isLoading) {
     return <p>Loading</p>;
   }
 
-  if (res.status !== "success") {
-    return <p>{res.message}</p>;
+  if (data.status !== "success") {
+    navigate("/hotels");
+    toast.error(`Error fetching the detail, ${data.message}`);
+    return;
   }
 
-  const { data: hotel } = res.data;
+  const { data: hotel } = data.data;
+  // console.log(hotel);
 
-  return <div>{hotel.name}</div>;
+  return (
+    <div className="flex flex-col gap-5">
+      <h1>{hotel.name}</h1>
+      <h3>starRating: {hotel.starRating}</h3>
+      <h3>address: {hotel.address}</h3>
+      <h3>summary: {hotel.summary}</h3>
+      <h3>description: {hotel.description}</h3>
+    </div>
+  );
 }
 
 export default HotelDetailsPage;
