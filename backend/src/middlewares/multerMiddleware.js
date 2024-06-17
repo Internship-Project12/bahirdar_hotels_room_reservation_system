c20c70add80c6792745d531fe8f2f119c6850195
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-17 18:52:57 +0300

    feat: multer middleware | add upload images function

--- backend/src/middlewares/multerMiddleware.js
+++ backend/src/middlewares/multerMiddleware.js
@@ -1,6 +1,25 @@
 import multer from 'multer';
 
 const storage = multer.memoryStorage();
-const upload = multer({ storage });
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5MB
+  },
+});
+
+export async function uploadImages(imageFiles) {
+  const uploadPromises = imageFiles.map(async (image) => {
+    const b64 = Buffer.from(image.buffer).toString('base64');
+    let dataURI = `data:${image.mimetype};base64,${b64}`;
+    const res = await cloudinary.v2.uploader.upload(dataURI);
+    // console.log('upload image ',res)
+
+    return res.url;
+  });
+
+  const imageUrls = await Promise.all(uploadPromises);
+  return imageUrls;
+}
 
 export default upload;
