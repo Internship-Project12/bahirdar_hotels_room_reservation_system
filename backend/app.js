c1f739bb6be37d16d64ebe2b6c2d58ea34e229b7
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-17 14:57:30 +0300

    feat: add express rate limit | mongo sanitize | helmet

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,10 @@ import express from 'express';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
 
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+
 import hotelRouter from './src/routes/hotelRoutes.js';
 import userRouter from './src/routes/userRoutes.js';
 import globalErrorHandlerMiddleWare from './src/middlewares/globalErrorHandlerMiddleWare.js';
@@ -19,11 +23,26 @@ process.on('uncaughtException', (err) => {
 
 const app = express();
 
+// Set security HTTP headers
+app.use(helmet());
+
 // if (process.env.NODE_ENV === 'development') {
 // }
 app.use(morgan('dev'));
 
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
 app.use(express.json());
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
 
 app.get('/api/v1/test', (req, res) => {
   res.status(200).json({
