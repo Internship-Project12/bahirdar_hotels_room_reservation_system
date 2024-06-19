36e0f71d6400d6fce16caf7bb65937f111661e13
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-19 14:42:22 +0300

    feat:frontend: app route udpated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import SignupPage from "./pages/SignupPage";
 import HotelsListPage from "./pages/HotelsListPage";
 import AddHotel from "./pages/AddHotel";
 import HotelDetailsPage from "./pages/HotelDetailsPage";
+import UpdateHotel from "./pages/UpdateHotel";
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
           <Route path="/hotels/:id" element={<HotelDetailsPage />} />
           <Route path="/booking/:id" element={<p>Hotel booking page</p>} />
           <Route path="/add-hotel" element={<AddHotel />} />
+          <Route path="/update-hotel/:id" element={<UpdateHotel />} />
           <Route path="/*" element={<Navigate to="/" />} />
         </Route>
       </Routes>
