315891abf3e9df8d1e7dec54545fccacaed6a075
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-27 13:24:05 +0300

    feat: config multer | add multer file filter

--- backend/src/middlewares/multerMiddleware.js
+++ backend/src/middlewares/multerMiddleware.js
@@ -1,9 +1,21 @@
 import multer from 'multer';
 import cloudinary from 'cloudinary';
+import AppError from '../utils/appError';
 
 const storage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (!file.mimetype.startsWith('image')) {
+    return cb(
+      new AppError('Not an image! Please upload images only images.', 400),
+      false
+    );
+  }
+};
+
 const upload = multer({
   storage,
+  fileFilter: multerFilter,
   // limits: {
   //   fileSize: 5 * 1024 * 1024, // 5MB
   // },
