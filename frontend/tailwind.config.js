4ea58afa5715c941eb2b0315de09e4550f8abf8a
fentahunM <fantahun360@gmial.com>
2024-06-29 00:44:31 +0300

    define custom grid template and font family

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        // Simple 8 column grid
+        10: "repeat(3, minmax(10%, 1fr)) 10% 10% 10% 20% 10% ",
+      },
+    },
+    lato: ["Lato", "sans-serif"],
   },
   plugins: [],
 };
