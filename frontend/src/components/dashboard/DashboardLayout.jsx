abe05f7857c32e6f0af5d585b632d0541d697e44
fentahunM <fantahun360@gmial.com>
2024-06-28 17:21:52 +0300

    style admin sidebar

--- frontend/src/components/dashboard/DashboardLayout.jsx
+++ frontend/src/components/dashboard/DashboardLayout.jsx
@@ -4,11 +4,11 @@ import Header from "./Header";
 
 function DashboardLayout() {
   return (
-    <div className="flex h-screen bg-gray-100">
+    <div className="mx-auto flex max-w-[1400px]">
       <SideBar />
-      <div className="flex flex-1 flex-col">
+      <div className="flex flex-1 flex-col bg-white">
         <Header />
-        <main className="flex-1 bg-gray-100 p-6">
+        <main className="flex-1s flex">
           <Outlet />
         </main>
       </div>
@@ -17,23 +17,3 @@ function DashboardLayout() {
 }
 
 export default DashboardLayout;
-
-// import Sidebar from "./SideBar";
-// import Header from "./../Header";
-// import { Outlet } from "react-router-dom";
-
-// const Layout = () => {
-//   return (
-//     <div className="flex h-screen">
-//       <Sidebar />
-//       <div className="flex flex-1 flex-col">
-//         <Header />
-//         <main className="flex-grow p-4">
-//           <Outlet />
-//         </main>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Layout;
--- frontend/src/components/dashboard/SideBar.jsx
+++ frontend/src/components/dashboard/SideBar.jsx
@@ -8,45 +8,45 @@ import {
 
 function SideBar() {
   return (
-    <div className="flex h-screen w-64 flex-col bg-gray-800 p-5 text-white">
-      <div className="mb-8 flex items-center gap-2">
+    <div className="flex h-screen w-[250px] flex-col gap-3 bg-gray-800 p-5 text-white">
+      <div className="flex items-center gap-4">
         <img
           className="h-16 w-16 rounded-full"
           src="hotel-images/img-2.jpg"
           alt="Logo"
         />
-        <h1 className="text-2xl font-bold">BDUHotels</h1>
+        <span className="text-2xl font-bold">BDUHotels</span>
       </div>
-      <nav className="flex flex-col space-y-4">
+      <nav className="flex flex-col gap-1 p-4 text-lg">
         <Link
-          className="flex items-center gap-2 rounded p-2 hover:bg-gray-700"
+          className="flex items-center gap-3 rounded p-3 hover:bg-slate-700"
           to="/"
         >
-          <HiOutlineHome />
+          <HiOutlineHome size={20} />
           <span>Home</span>
         </Link>
 
         <Link
-          className="flex items-center gap-2 rounded p-2 hover:bg-gray-700"
+          className="flex items-center gap-3 rounded p-3 hover:bg-slate-700"
           to="/hotels"
         >
-          <HiBuildingLibrary />
+          <HiBuildingLibrary size={20} />
           <span>Hotels</span>
         </Link>
 
         <Link
-          className="flex items-center gap-2 rounded p-2 hover:bg-gray-700"
+          className="flex items-center gap-3 rounded p-3 hover:bg-slate-700"
           to="/bookings"
         >
-          <HiOutlineCalendarDays />
+          <HiOutlineCalendarDays size={20} />
           <span>Bookings</span>
         </Link>
 
         <Link
-          className="flex items-center gap-2 rounded p-2 hover:bg-gray-700"
+          className="flex items-center gap-3 rounded p-3 hover:bg-slate-700"
           to="/users"
         >
-          <HiOutlineUsers />
+          <HiOutlineUsers size={20} />
           <span>Users</span>
         </Link>
       </nav>
@@ -55,3 +55,47 @@ function SideBar() {
 }
 
 export default SideBar;
+
+// <div className="fixed left-0 top-0 flex h-screen w-64 flex-col bg-gray-800 p-5 text-white">
+//       <div className="mb-8 flex items-center gap-2">
+//         <img
+//           className="h-16 w-16 rounded-full"
+//           src="hotel-images/img-2.jpg"
+//           alt="Logo"
+//         />
+//         <h1 className="text-2xl font-bold">BDUHotels</h1>
+//       </div>
+//       <nav className="flex flex-col space-y-4">
+//         <Link
+//           className="flex items-center gap-2 rounded p-2 hover:bg-gray-700"
+//           to="/"
+//         >
+//           <HiOutlineHome />
+//           <span>Home</span>
+//         </Link>
+
+//         <Link
+//           className="flex items-center gap-2 rounded p-2 hover:bg-gray-700"
+//           to="/hotels"
+//         >
+//           <HiBuildingLibrary />
+//           <span>Hotels</span>
+//         </Link>
+
+//         <Link
+//           className="flex items-center gap-2 rounded p-2 hover:bg-gray-700"
+//           to="/bookings"
+//         >
+//           <HiOutlineCalendarDays />
+//           <span>Bookings</span>
+//         </Link>
+
+//         <Link
+//           className="flex items-center gap-2 rounded p-2 hover:bg-gray-700"
+//           to="/users"
+//         >
+//           <HiOutlineUsers />
+//           <span>Users</span>
+//         </Link>
+//       </nav>
+//     </div>
