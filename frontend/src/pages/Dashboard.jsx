de12a36e794aec9ff0273b8b9341fc85cfda9708
fentahunM <fantahun360@gmial.com>
2024-06-29 23:52:38 +0300

    build dashboard's hotel stat section

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,55 @@
+import {
+  MdOutlineBedroomChild,
+  MdOutlineFreeCancellation,
+  MdOutlineManageSearch,
+  MdOutlinePendingActions,
+  MdOutlineShoppingCartCheckout,
+} from "react-icons/md";
+import HotelsStat from "../components/hotels/HotelsStat";
+
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div className="flex w-full flex-col p-3">
+      <section className="grid grid-cols-4 justify-between">
+        <HotelsStat
+          Icon={MdOutlineBedroomChild}
+          title={"Available rooms"}
+          number={130}
+        />
+
+        <HotelsStat
+          Icon={MdOutlineShoppingCartCheckout}
+          title={"Today Checkouts"}
+          number={130}
+        />
+
+        <HotelsStat
+          Icon={MdOutlineFreeCancellation}
+          title={"Cancellations"}
+          number={21}
+        />
+
+        <HotelsStat
+          Icon={MdOutlineManageSearch}
+          title={"Enquiries"}
+          number={412}
+        />
+
+        <HotelsStat
+          Icon={MdOutlinePendingActions}
+          title={"Pending Payments"}
+          number={78}
+        />
+      </section>
+
+      <div>
+        <h2>Recent Enquiries</h2>
+        <h2>Booking Status</h2>
+      </div>
+      <h2>Available Rooms</h2>
+      <h2>Booking Detials</h2>
+    </div>
+  );
 }
 
 export default Dashboard;
