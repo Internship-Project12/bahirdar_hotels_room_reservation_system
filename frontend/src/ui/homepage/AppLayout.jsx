aee1a3fd74b44df7afb5258a0f73a246cfe1a37a
fentahunM <fantahun360@gmial.com>
2024-07-02 21:09:27 +0300

    refactor folder structure

---
+++ frontend/src/ui/homepage/AppLayout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Header from "../Header";
+import Footer from "../Footer";
+
+function AppLayout() {
+  return (
+    <div className="flex min-h-screen flex-col gap-4">
+      <Header />
+      <main className="h-[100%] flex-1">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default AppLayout;
