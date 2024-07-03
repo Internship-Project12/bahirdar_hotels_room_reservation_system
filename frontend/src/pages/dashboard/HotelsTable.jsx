8e93a7967b401f3fdefdea35eea4b27d16f17ffa
fentahunM <fantahun360@gmial.com>
2024-07-03 05:55:43 +0300

    button to add hotel

--- frontend/src/pages/dashboard/HotelsTable.jsx
+++ frontend/src/pages/dashboard/HotelsTable.jsx
@@ -5,9 +5,14 @@ import HotelTableHeading from "../../features/hotels/HotelTableHeading";
 function HotelsTable() {
   return (
     <div className="w-full bg-white font-lato text-gray-600 shadow-md">
-      <h1 className="p-4 font-opensans text-2xl font-bold uppercase">
-        All Hotels
-      </h1>
+      <div className="flex items-center justify-between">
+        <h1 className="p-4 font-opensans text-2xl font-bold uppercase">
+          All Hotels
+        </h1>
+        <button className="mr-2 cursor-pointer rounded bg-blue-700 px-4 py-[6px] text-lg text-white">
+          Add Hotel
+        </button>
+      </div>
       <HotelTableHeading />
 
       {hotels.map((hotel) => (
