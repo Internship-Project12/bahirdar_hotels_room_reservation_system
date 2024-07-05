5ed2638d3ba1a03b9d94505b44257d83e86d6569
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-07-05 13:12:34 +0300

    feat: implement update user controller

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -2,6 +2,11 @@ import User from '../models/userModel.js';
 import AppError from '../utils/appError.js';
 import catchAsync from '../utils/catchAsync.js';
 
+export const getMe = (req, res, next) => {
+  req.params.id = req.user._id;
+  next();
+};
+
 export const getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
 
@@ -30,6 +35,7 @@ export const getUser = catchAsync(async (req, res, next) => {
   const { id } = req.params;
 
   const user = await User.findById(id);
+
   if (!user) return next(new AppError('No user found with that ID', 404));
 
   res.status(200).json({
@@ -41,13 +47,23 @@ export const getUser = catchAsync(async (req, res, next) => {
   });
 });
 
-export const getMe = (req, res, next) => {
-  req.params.id = req.user._id;
-  next();
-};
-
 export const updateUser = async (req, res) => {
-  res.status(200).json({ message: 'Update user' });
+  const { id } = req.params;
+
+  const user = await User.findByIdAndUpdate(id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!user) return next(new AppError('No user found with that ID', 404));
+
+  res.status(200).json({
+    status: 'success',
+    message: 'Update user',
+    data: {
+      user,
+    },
+  });
 };
 
 export const deleteUser = async (req, res) => {
