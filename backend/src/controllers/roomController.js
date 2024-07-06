b530f86e5c87f11f1ca85df710b7ebb86c2b8cd4
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-07-06 11:10:37 +0300

    feat: add a room controller

---
+++ backend/src/controllers/roomController.js
@@ -0,0 +1,46 @@
+import catchAsync from '../utils/catchAsync.js';
+
+const getAllRooms = catchAsync(async (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'get all routes',
+  });
+});
+
+const getRoom = catchAsync(async (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'get one route',
+  });
+});
+
+const createRoom = catchAsync(async (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'create one route',
+  });
+});
+
+const updateRoom = catchAsync(async (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'update one route',
+  });
+});
+
+const deleteRoom = catchAsync(async (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'delete one route',
+  });
+});
+
+const roomController = {
+  getAllRooms,
+  getRoom,
+  createRoom,
+  updateRoom,
+  deleteRoom,s
+};
+
+export default roomController;
