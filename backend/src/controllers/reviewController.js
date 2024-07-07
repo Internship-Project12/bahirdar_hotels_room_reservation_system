6fbc10bfb7b94c4c3b6c75a4b050fae8f448831d
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-07-07 21:23:09 +0300

    feat: implement get all review controller

--- backend/src/controllers/reviewController.js
+++ backend/src/controllers/reviewController.js
@@ -1,9 +1,22 @@
+import APIFeatures from '../utils/apiFeatures.js';
 import catchAsync from '../utils/catchAsync.js';
+import Review from '../models/reviewModel.js';
 
 const getAllReviews = catchAsync(async (req, res, next) => {
+  const features = new APIFeatures(Review.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+
+  const reviews = await features.query;
+
   res.status(200).json({
     status: 'success',
     message: 'get all reviews',
+    data: {
+      reviews,
+    },
   });
 });
 
