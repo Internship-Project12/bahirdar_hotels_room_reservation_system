ef01954bb61dcf15852d49e94cb67590b58e280a
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-07-07 11:48:04 +0300

    feat: implement get all bookings

--- backend/src/controllers/bookingController.js
+++ backend/src/controllers/bookingController.js
@@ -1,9 +1,23 @@
+import Booking from '../models/bookingModel.js';
+import APIFeatures from '../utils/apiFeatures.js';
 import catchAsync from '../utils/catchAsync.js';
 
 const getAllBookings = catchAsync(async (req, res, next) => {
+  const features = new APIFeatures(Booking.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+
+  const bookings = await features.query;
+
   res.status(200).json({
     status: 'success',
     message: 'get all bookings',
+    numOfBookings: bookings.length,
+    data: {
+      bookings,
+    },
   });
 });
 
