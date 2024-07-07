311ad4e6e37d0146e7cf9b8e0a2ae0dd1751bcca
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-07-07 21:14:54 +0300

    feat: create review routes

---
+++ backend/src/routes/reviewRoutes.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import reviewController from '../controllers/reviewController.js';
+
+const router = express.Router({ mergeParams: true });
+
+router
+  .route('/')
+  .get(reviewController.getAllReviews)
+  .post(reviewController.createReview);
+
+router
+  .route('/:id')
+  .get(reviewController.getReview)
+  .patch(reviewController.updateReview)
+  .delete(reviewController.deleteReview);
+
+export default router;
