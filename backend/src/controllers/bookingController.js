b8b0b16ec3685dc5bfccff6d9c618960e8b00ce1
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-07-07 12:07:57 +0300

    feat: implement the booking controller | crud

--- backend/src/controllers/bookingController.js
+++ backend/src/controllers/bookingController.js
@@ -1,5 +1,6 @@
 import Booking from '../models/bookingModel.js';
 import APIFeatures from '../utils/apiFeatures.js';
+import AppError from '../utils/appError.js';
 import catchAsync from '../utils/catchAsync.js';
 
 const getAllBookings = catchAsync(async (req, res, next) => {
@@ -22,30 +23,69 @@ const getAllBookings = catchAsync(async (req, res, next) => {
 });
 
 const getBooking = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+
+  const booking = await Booking.findById(id);
+
+  if (!booking) {
+    return next(new AppError('there is no booking found with that id', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     message: 'get booking',
+    data: {
+      booking,
+    },
   });
 });
 
 const updateBooking = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+
+  const booking = await Booking.findByIdAndUpdate(id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!booking) {
+    return next(new AppError('there is no booking found with that id', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     message: 'update bookings',
+    data: {
+      booking,
+    },
   });
 });
 
 const createBooking = catchAsync(async (req, res, next) => {
+  const booking = await Booking.create(req.body);
+
   res.status(200).json({
     status: 'success',
     message: 'create booking',
+    data: {
+      booking,
+    },
   });
 });
 
 const deleteBooking = catchAsync(async (req, res, next) => {
-  res.status(200).json({
+  const { id } = req.params;
+
+  const booking = await Booking.findByIdAndDelete(id);
+
+  if (!booking) {
+    return next(new AppError('there is no booking found with that id', 404));
+  }
+
+  res.status(204).json({
     status: 'success',
     message: 'delete booking',
+    data: null,
   });
 });
 
