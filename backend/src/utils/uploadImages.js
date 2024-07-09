61f1dffadf6dad4d60b6ab977dd8110fcfe92961
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-07-09 13:42:06 +0300

    feat: add an uploadSingleImage fns

--- backend/src/utils/uploadImages.js
+++ backend/src/utils/uploadImages.js
@@ -1,13 +1,31 @@
 import cloudinary from 'cloudinary';
+import AppError from './appError.js';
 
 export async function uploadImages(imageFiles) {
-  const uploadPromises = imageFiles.map(async (image) => {
+  try {
+    const uploadPromises = imageFiles.map(async (image) => {
+      const b64 = Buffer.from(image.buffer).toString('base64');
+      let dataURI = `data:${image.mimetype};base64,${b64}`;
+      const res = await cloudinary.v2.uploader.upload(dataURI);
+      return res.url;
+    });
+
+    const imageUrls = await Promise.all(uploadPromises);
+    return imageUrls;
+  } catch (error) {
+    console.error('🔥', error);
+    return next(new AppError('Unable to upload images, try again', 500));
+  }
+}
+
+export async function uploadSingleImage(image) {
+  try {
     const b64 = Buffer.from(image.buffer).toString('base64');
     let dataURI = `data:${image.mimetype};base64,${b64}`;
     const res = await cloudinary.v2.uploader.upload(dataURI);
     return res.url;
-  });
-
-  const imageUrls = await Promise.all(uploadPromises);
-  return imageUrls;
+  } catch (error) {
+    console.error('🔥', error);
+    return next(new AppError('Unable to upload photo, Please try again', 500));
+  }
 }
