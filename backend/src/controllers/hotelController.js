5bed0339a032e81f8ceb0256f03558f5ca5ea15f
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-07-09 13:43:19 +0300

    fix: the condition to upload images on the hotel route

--- backend/src/controllers/hotelController.js
+++ backend/src/controllers/hotelController.js
@@ -31,14 +31,9 @@ export const createHotel = catchAsync(async (req, res, next) => {
   let imageCoverUrl;
   let hotelImagesUrl;
 
-  try {
-    if (!req.files?.imageCoverFile || !req.files?.hotelImagesFiles) {
-      imageCoverUrl = await uploadImages(req.files.imageCoverFile);
-      hotelImagesUrl = await uploadImages(req.files.hotelImagesFiles);
-    }
-  } catch (error) {
-    console.error('🔥', error);
-    return next(new AppError('Unable to upload images, try again', 500));
+  if (req.files?.imageCoverFile || req.files?.hotelImagesFiles) {
+    imageCoverUrl = await uploadImages(req.files.imageCoverFile);
+    hotelImagesUrl = await uploadImages(req.files.hotelImagesFiles);
   }
 
   const hotel = await Hotel.create({
