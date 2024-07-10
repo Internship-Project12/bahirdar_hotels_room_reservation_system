e1ab8a287c35bb900ee27cce0aa8199c53aa0fcf
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-07-10 12:16:35 +0300

    feat: update constants | default user image

--- backend/src/constants/constants.js
+++ backend/src/constants/constants.js
@@ -3,3 +3,6 @@ export const BookingStatus = {
   CONFIRMED: 'confirmed',
   CANCELLED: 'cancelled',
 };
+
+export const DEFAULT_USER_AVATAR =
+  'https://res.cloudinary.com/dvp1mjhd9/image/upload/v1714759095/gsqg5uwxwtzc744wy6j5.png';
