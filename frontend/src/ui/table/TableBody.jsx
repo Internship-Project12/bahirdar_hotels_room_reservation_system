77fb5ab645004c192b3217c61e638ab532192664
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-07-12 17:41:13 +0300

    feat: table body | display based role

--- frontend/src/ui/table/TableBody.jsx
+++ frontend/src/ui/table/TableBody.jsx
@@ -3,27 +3,37 @@
 function TableBody({ headers, data }) {
   return (
     <div
-      className={`mb-2 grid grid-cols-${headers.length} items-center gap-3 border-b border-l border-r border-slate-200 p-2 text-sm`}
+      className={`relative mb-2 grid grid-cols-${headers.length} items-start gap-3 border-b border-l border-r border-slate-200 p-2 text-sm`}
     >
       {headers.map((col, i) => (
         <div key={i} className="col-span-1 flex">
           {renderCell(data, col.key)}
         </div>
       ))}
+      <div className="absolute right-4 text-right">
+        {data?.role && (
+          <>
+            <p className="rounded inline-block bg-blue-800 px-2 py-[2px] text-white">
+              {data?.role}
+            </p>
+            <p className="">{data.role === "manager" && data.hotel}</p>
+          </>
+        )}
+      </div>
     </div>
   );
 }
 
 function renderCell(data, key) {
-  if (key === "customer") {
+  if (key === "user" && data.userImg) {
     return (
       <div className="flex items-center gap-2">
         <img
           className="h-12 w-12 rounded-full object-cover"
-          src={data.customerImg}
-          alt={data.customer}
+          src={data.userImg}
+          alt={data.user}
         />
-        <span>{data.customer}</span>
+        <span>{data.user}</span>
       </div>
     );
   }
