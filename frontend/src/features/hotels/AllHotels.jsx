40a7fcbea6786e77b0611ea94c55bc04f861a8a7
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-07-13 10:03:38 +0300

    feat: use query | fetch and pass all hotels

--- frontend/src/features/hotels/AllHotels.jsx
+++ frontend/src/features/hotels/AllHotels.jsx
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom";
-import { hotels } from "../../data/hotels";
 import HotelTableBody from "./HotelTableBody";
-// import HotelTableHeading from "./HotelTableHeading";
+import HotelTableHeading from "./HotelTableHeading";
+import { useHotels } from "./useHotels";
 
 function AllHotels() {
+  const { data, isLoading } = useHotels();
+
+  if (isLoading) return <div>Loading all hotels</div>;
+  const { data: hotels } = data.data;
+
   return (
     <div className="w-full bg-white font-lato text-gray-600 shadow-md">
       <div className="flex items-center justify-between">
-        <h1 className="p-4  text-2xl font-bold uppercase">
-          All Hotels
-        </h1>
+        <h1 className="p-4 text-2xl font-bold uppercase">All Hotels</h1>
         <Link
           to={"/dashboard/add-hotel"}
           className="mr-2 cursor-pointer rounded bg-blue-700 px-4 py-[6px] text-lg text-white"
@@ -18,7 +21,7 @@ function AllHotels() {
         </Link>
       </div>
 
-      {/* <HotelTableHeading /> */}
+      <HotelTableHeading />
 
       {hotels.map((hotel) => (
         <HotelTableBody key={hotel.id} hotel={hotel} />
