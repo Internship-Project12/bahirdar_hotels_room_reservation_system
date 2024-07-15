3e0aef81fc1adf55b4e74a30614ca185c70401cc
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-07-15 21:30:37 +0300

    feat: api rooms | create room

--- frontend/src/services/apiRoom.js
+++ frontend/src/services/apiRoom.js
@@ -10,6 +10,16 @@ const getAllRooms = async (id) => {
   return res.data;
 };
 
-const apiRooms = { getAllRooms };
+const createRoom = async ({ id, data }) => {
+  let url = `/rooms`;
+  if (id) {
+    url = `/hotels/${id}/rooms`;
+  }
+
+  const res = await customFetch.post(url, data);
+  return res.data;
+};
+
+const apiRooms = { getAllRooms, createRoom };
 
 export default apiRooms;
