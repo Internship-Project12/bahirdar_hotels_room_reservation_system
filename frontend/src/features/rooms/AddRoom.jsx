c8754664e92ff9a537d36b91a531c1c78d3a218b
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-07-16 16:29:09 +0300

    feat: refactor form on add room | to handle updating

--- frontend/src/features/rooms/AddRoom.jsx
+++ frontend/src/features/rooms/AddRoom.jsx
@@ -1,18 +1,5 @@
-import { FormProvider, useForm } from "react-hook-form";
-import SpinnerMini from "../../ui/SpinnerMini";
 import { useCreateRoom } from "./useCreateRoom";
-
-import {
-  RoomAmenities,
-  RoomImages,
-  RoomDescription,
-  RoomCapacity,
-  RoomPricePerNight,
-  RoomType,
-  RoomNumber,
-} from "../../forms/manageRoomForm";
-import { useRooms } from "./useRooms";
-import Spinner from "../../ui/Spinner";
+import ManageRoomForm from "../../forms/manageRoomForm/ManageRoomForm";
 
 /*
   {
@@ -45,80 +32,12 @@ import Spinner from "../../ui/Spinner";
 
 function AddRoom() {
   const { mutate, isPending } = useCreateRoom();
-  const { data: {data: {rooms} = {}} = {}, isLoading } = useRooms();
 
-  const formMethods = useForm();
-  const { handleSubmit } = formMethods;
+  const onSubmitHandler = (room) => {
+    mutate(room);
+  };
 
-  const onSubmitHandler = handleSubmit((data) => {
-    const formData = new FormData();
-
-    formData.append("roomNumber", data.roomNumber);
-    formData.append("roomType", data.roomType);
-    formData.append("pricePerNight", data.pricePerNight);
-    formData.append("capacity", data.capacity);
-    formData.append("description", data.description);
-
-    data.amenities.forEach((amenity, i) => {
-      formData.append(`amenities[${i}]`, amenity);
-    });
-
-    if (data?.RoomImageFiles) {
-      Array.from(data.RoomImageFiles).forEach((image) => {
-        formData.append("RoomImageFiles", image);
-      });
-    }
-
-    mutate(formData);
-  });
-
-  if (isLoading) return <Spinner />;
-
-  return (
-    <FormProvider {...formMethods}>
-      <div className="flex items-center justify-center p-3">
-        <h1 className="min-w-[30rem] cursor-pointer rounded-full bg-blue-600 px-6 py-2 text-center text-2xl font-bold text-white shadow-xl">
-          {"Add Room"}
-          {/* {isInUpdateMode ? "update hotel" : "Add Hotel"} */}
-        </h1>
-      </div>
-      <form
-        onSubmit={onSubmitHandler}
-        className="m-auto flex flex-col gap-8 rounded bg-slate-100 p-10 shadow-lg"
-      >
-        <div className="flex flex-col gap-4">
-          {/* ROOM NUMBER */}
-          <RoomNumber rooms={rooms}/>
-
-          {/* ROOM TYPE */}
-          <RoomType />
-
-          {/* PRICE PER NIGHT */}
-          <RoomPricePerNight />
-
-          {/* CAPACITY */}
-          <RoomCapacity />
-
-          {/* ROOM DESCRIPTION */}
-          <RoomDescription />
-
-          {/* ROOM AMENITIES */}
-          <RoomAmenities />
-
-          {/* ROOM IMAGES */}
-          <RoomImages />
-        </div>
-
-        <button
-          type="submit"
-          className="w-full rounded bg-blue-800 px-3 py-2 text-white transition-all duration-300 hover:bg-blue-700 disabled:cursor-not-allowed disabled:bg-blue-600"
-          disabled={isPending}
-        >
-          {isPending ? <SpinnerMini /> : "Save Room"}
-        </button>
-      </form>
-    </FormProvider>
-  );
+  return <ManageRoomForm onSubmit={onSubmitHandler} isAdding={isPending} />;
 }
 
 export default AddRoom;
