244480af9e98cda3b1210bff65083a79ae2ef110
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-07-17 20:19:34 +0300

    style: all hotels list page | search compoenent

--- frontend/src/ui/Search.jsx
+++ frontend/src/ui/Search.jsx
@@ -1,5 +1,10 @@
 /* eslint-disable react/prop-types */
-function Search({ onSearchHandler, isLoading = false, register }) {
+function Search({ onSearchHandler, isLoading = false, register, size = {} }) {
+  // we change the size of the search box on the hotels list page so size prop is sent to make reuseable
+  let style = "";
+  if (size?.height && size?.width) {
+    style = `h-[${size.height}rem] w-[${size.width}rem]`;
+  }
   return (
     <form
       className="group flex items-center justify-center"
@@ -10,14 +15,14 @@ function Search({ onSearchHandler, isLoading = false, register }) {
           type="search"
           disabled={isLoading}
           autoFocus
-          className="rounded-full bg-slate-200 px-3 py-2 focus:outline-none disabled:cursor-not-allowed"
+          className={`rounded-full bg-slate-200 px-3 py-2 focus:outline-none disabled:cursor-not-allowed ${style}`}
           placeholder="Search"
           {...register("search")}
         />
         <button
           type="submit"
           disabled={isLoading}
-          className="-ml-16 w-24 rounded-full bg-blue-600 px-3 py-2 text-white disabled:cursor-not-allowed disabled:bg-blue-400"
+          className="-ml-20 w-[8rem] rounded-full bg-blue-600 px-3 py-2 text-white disabled:cursor-not-allowed disabled:bg-blue-400"
         >
           Search
         </button>
