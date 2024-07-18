5fa8e77212bfdf9a1923e525b50adc9b44fcb51d
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-07-18 17:27:04 +0300

    feat: add a new component | room type filter

---
+++ frontend/src/components/RoomTypeFilter.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+function RoomTypeFilter({ selectedRoomTypes, onChange }) {
+  return (
+    <div className="border-b border-slate-300 pb-5">
+      <h2>Filter By Room Type:</h2>
+      {["single", "double", "twin", "triple", "quad", "twin-double"].map(
+        (type) => (
+          <label className="mt-2 flex items-center space-x-2" key={type}>
+            <input
+              type="checkbox"
+              className="rounded accent-blue-600"
+              value={type}
+              checked={selectedRoomTypes.includes(type)}
+              onChange={onChange}
+            />
+            <span>{type}</span>
+          </label>
+        ),
+      )}
+    </div>
+  );
+}
+
+export default RoomTypeFilter;
