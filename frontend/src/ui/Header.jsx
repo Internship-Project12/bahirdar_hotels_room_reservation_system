07472b87c30b66c72421800644232cfe7c20398a
fentahunM <fantahun360@gmial.com>
2024-07-19 02:08:53 +0300

    fix font weight

--- frontend/src/ui/Header.jsx
+++ frontend/src/ui/Header.jsx
@@ -9,11 +9,11 @@ function Header() {
     <header className="z-10 flex h-[15vh] min-w-full items-center justify-between bg-blue-900 px-24 py-6 opacity-85">
       <Logo />
       <nav>
-        <ul className="flex items-center justify-between gap-4">
+        <ul className="flex items-center justify-between gap-4 text-lg">
           <li>
             <Link
               to="/hotels"
-              className="rounded px-3 py-2 font-bold text-blue-100 underline"
+              className="rounded px-3 py-2 font-bold text-blue-100 hover:text-[#05bd05]"
             >
               Hotels
             </Link>
@@ -21,7 +21,7 @@ function Header() {
           <li>
             <Link
               to="/about"
-              className="px-3 py-2 font-bold text-blue-100 underline"
+              className="px-3 py-2 font-bold text-blue-100 hover:text-[#05bd05]"
             >
               About
             </Link>
@@ -35,7 +35,7 @@ function Header() {
             <li>
               <Link
                 to="/login"
-                className="rounded bg-slate-200 px-3 py-2 font-bold text-blue-800"
+                className="rounded bg-slate-200 px-3 py-2 font-bold text-blue-800 hover:text-[#05bd05]"
               >
                 Sign In
               </Link>
