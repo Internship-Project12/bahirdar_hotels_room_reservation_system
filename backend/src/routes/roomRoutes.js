27882b47d34fcfd4712fe1dbf50abc664b77df8b
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-07-20 15:28:18 +0300

    feat: room router | nested route | get all booking on room

--- backend/src/routes/roomRoutes.js
+++ backend/src/routes/roomRoutes.js
@@ -2,8 +2,11 @@ import express from 'express';
 import roomController from '../controllers/roomController.js';
 import authController from '../controllers/authController.js';
 import upload from '../middlewares/multerMiddleware.js';
+import bookingRouter from './bookingRoutes.js';
 
 const router = express.Router({ mergeParams: true });
+
+router.use('/:roomId/bookings', bookingRouter);
 
 router
   .route('/')
