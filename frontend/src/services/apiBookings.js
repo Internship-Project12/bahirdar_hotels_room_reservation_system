fd840a9c74e2eea73cb46d8a8ca708e30effd81b
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-07-20 16:44:03 +0300

    feat: api bookings | get all booking on room

--- frontend/src/services/apiBookings.js
+++ frontend/src/services/apiBookings.js
@@ -3,20 +3,29 @@ import customFetch from "../utils/customFetch";
 const getAllBookings = async ({ filter }) => {
   const { status, hotelId } = filter;
 
-  const res = await customFetch(`/bookings?status=${status}&hotel=${hotelId}`);
+  const res = await customFetch.get(
+    `/bookings?status=${status}&hotel=${hotelId}`,
+  );
+
+  return res.data;
+};
+
+const getAllBookingsOnRoom = async ({ roomId }) => {
+  const res = await customFetch.get(`/rooms/${roomId}/bookings`);
 
   return res.data;
 };
 
 const createBooking = async (data) => {
-  const res = await customFetch.post('/bookings', data)
+  const res = await customFetch.post("/bookings", data);
 
-  return res.data
-}
+  return res.data;
+};
 
 const apiBookings = {
   getAllBookings,
   createBooking,
+  getAllBookingsOnRoom,
 };
 
 export default apiBookings;
