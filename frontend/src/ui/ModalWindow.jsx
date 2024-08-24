03e5eaeb4c6696b18a84cbf4ab23ca126f738683
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-08-24 09:46:12 +0300

    feat: create portal on modal window

--- frontend/src/ui/ModalWindow.jsx
+++ frontend/src/ui/ModalWindow.jsx
@@ -1,3 +1,4 @@
+import { createPortal } from 'react-dom'
 import { useAuthContext } from "../context/AuthContext";
 import { MdCancel } from "react-icons/md";
 
@@ -5,13 +6,12 @@ import { MdCancel } from "react-icons/md";
 function ModalWindow({ children }) {
   const { handleOpenModalWindow } = useAuthContext();
 
-  return (
-    <div className="absolute inset-0 left-0 top-0 z-[100] flex items-center justify-center">
-      <div
-        onClick={handleOpenModalWindow}
-        className="absolute inset-0 left-0 top-0 bg-black/30 backdrop-blur-3xl hover:cursor-pointer"
-      ></div>
-      <div className="relative max-h-[90vh] max-w-[70vw] translate-x-4 overflow-y-auto overflow-x-hidden rounded-xl border bg-slate-100 shadow-xl">
+  return createPortal(
+    <div
+      onClick={handleOpenModalWindow}
+      className="fixed inset-0 left-0 top-0 z-10 h-screen w-full bg-black/30 backdrop-blur-3xl duration-300 hover:cursor-pointer"
+    >
+      <div className="fixed left-[50%] top-[50%] -translate-y-[50%] border bg-slate-100 shadow-xl">
         <button
           className="absolute right-3 top-2 rounded-full shadow"
           onClick={handleOpenModalWindow}
@@ -22,10 +22,79 @@ function ModalWindow({ children }) {
           />
         </button>
 
-        {children}
+        <div>{children}</div>
       </div>
-    </div>
+    </div>,
+    document.getElementById("modal"),
   );
 }
 
 export default ModalWindow;
+
+/*
+
+ function Modal({children, onClose}) {
+  return (
+    <Overlay>
+      <StyledModal>
+        <Button onClick={onClose}>
+          <HiXMark />
+        </Button>
+
+        <div>{children}</div>
+      </StyledModal>
+    </Overlay>
+  )
+ }
+
+
+const StyledModal = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: var(--color-grey-0);
+  border-radius: var(--border-radius-lg);
+  box-shadow: var(--shadow-lg);
+  padding: 3.2rem 4rem;
+  transition: all 0.5s;
+`;
+
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: var(--backdrop-color);
+  backdrop-filter: blur(4px);
+  z-index: 1000;
+  transition: all 0.5s;
+`;
+
+const Button = styled.button`
+  background: none;
+  border: none;
+  padding: 0.4rem;
+  border-radius: var(--border-radius-sm);
+  transform: translateX(0.8rem);
+  transition: all 0.2s;
+  position: absolute;
+  top: 1.2rem;
+  right: 1.9rem;
+
+  &:hover {
+    background-color: var(--color-grey-100);
+  }
+
+  & svg {
+    width: 2.4rem;
+    height: 2.4rem;
+    // // Sometimes we need both
+    // fill: var(--color-grey-500);
+    // stroke: var(--color-grey-500);
+    color: var(--color-grey-500);
+  }
+`;
+
+*/
