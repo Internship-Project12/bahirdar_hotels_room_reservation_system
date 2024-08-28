df826dd9dcb0889889991ba42de80c6da8228f49
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-08-28 19:20:16 +0300

    feat: finish implementing update my password functionality

--- frontend/src/features/profile/UpdateMyPassword.jsx
+++ frontend/src/features/profile/UpdateMyPassword.jsx
@@ -1,9 +1,8 @@
 import { FormProvider, useForm } from "react-hook-form";
-import { useAuthContext } from "../../context/AuthContext";
 import SpinnerMini from "../../ui/SpinnerMini";
+import { useUpdateMyPassword } from "./useUpdateMyPassword";
 
 function UpdateMyPassword() {
-  const { user } = useAuthContext();
   const formMethods = useForm();
   const {
     register,
@@ -11,11 +10,12 @@ function UpdateMyPassword() {
     handleSubmit,
   } = formMethods;
 
+  const { mutate, isPending } = useUpdateMyPassword();
+
   const onSubmitHandler = handleSubmit((data) => {
     console.log(data);
+    mutate({ data });
   });
-
-  const isPending = true;
 
   return (
     <div className="mx-auto mt-10 max-w-4xl rounded-md bg-white p-6 shadow-md">
@@ -28,36 +28,73 @@ function UpdateMyPassword() {
                   Old Password
                   <input
                     type="password"
-                    {...register("firstName")}
-                    defaultValue={user.passwordCurrent}
+                    {...register("passwordCurrent", {
+                      validate: (val) => {
+                        if (!val) return "old password is required.";
+                        else if (val.length < 8)
+                          return "password must be at least 8 characters.";
+
+                        return true;
+                      },
+                    })}
                     disabled={isPending}
                     className="mt-1 block w-full rounded-md border border-gray-300 p-2 shadow-sm focus:border-blue-300 focus:outline-none focus:ring disabled:cursor-wait disabled:bg-slate-300"
                   />
                 </label>
+                {errors.passwordCurrent && (
+                  <p className="mt-1 text-sm text-red-500">
+                    {errors.passwordCurrent.message}
+                  </p>
+                )}
               </div>
               <div className="mb-4">
                 <label className="block text-sm font-medium text-gray-700">
                   New Password
                   <input
                     type="password"
-                    {...register("firstName")}
-                    defaultValue={user.passwordCurrent}
+                    {...register("password", {
+                      validate: (val) => {
+                        if (!val) return "new password is required.";
+                        else if (val.length < 8)
+                          return "password must be at least 8 characters.";
+
+                        return true;
+                      },
+                    })}
                     disabled={isPending}
                     className="mt-1 block w-full rounded-md border border-gray-300 p-2 shadow-sm focus:border-blue-300 focus:outline-none focus:ring disabled:cursor-wait disabled:bg-slate-300"
                   />
                 </label>
+                {errors.password && (
+                  <p className="mt-1 text-sm text-red-500">
+                    {errors.password.message}
+                  </p>
+                )}
               </div>
               <div className="mb-4">
                 <label className="block text-sm font-medium text-gray-700">
                   Password Confirm
                   <input
                     type="password"
-                    {...register("firstName")}
-                    defaultValue={user.passwordCurrent}
+
+                    {...register("passwordConfirm", {
+                      validate: (val) => {
+                        if (!val) return "password confirm is required.";
+                        else if (val !== formMethods.getValues("password"))
+                          return "password confirm must match the password.";
+
+                        return true;
+                      },
+                    })}
                     disabled={isPending}
                     className="mt-1 block w-full rounded-md border border-gray-300 p-2 shadow-sm focus:border-blue-300 focus:outline-none focus:ring disabled:cursor-wait disabled:bg-slate-300"
                   />
                 </label>
+                {errors.passwordConfirm && (
+                  <p className="mt-1 text-sm text-red-500">
+                    {errors.passwordConfirm.message}
+                  </p>
+                )}
               </div>
               <div className="my-2 flex justify-end p-2">
                 <button
