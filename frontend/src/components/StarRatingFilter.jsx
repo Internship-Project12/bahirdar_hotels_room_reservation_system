858dbf27c202d4d0a9947b252f82b317b3c4d7e5
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-11-01 15:41:00 +0300

    feat: enhance HotelsListPage | add sorting functionality and refactor layout, remove unused HotelListFilter component

--- frontend/src/components/StarRatingFilter.jsx
+++ frontend/src/components/StarRatingFilter.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 function StarRatingFilter({ selectedStars, onChange }) {
   return (
-    <div className="border-b border-slate-300 pb-5">
+    <div className="pb-5">
       <h2>Filter By Hotel Star:</h2>
       {["5", "4", "3", "2", "1"].map((star) => (
         <label className="mt-2 flex items-center space-x-2" key={star}>
--- frontend/src/pages/HotelsListPage.jsx
+++ frontend/src/pages/HotelsListPage.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Link, useNavigate, useSearchParams } from "react-router-dom";
-
+import Select from "react-select";
 import { useHotels } from "../features/hotels/useHotels";
 import HotelListFilter from "../ui/HotelListFilter";
 import HotelsListItem from "../ui/HotelsListItem";
 import StarRatingFilter from "../components/StarRatingFilter";
-import Search from "../ui/Search";
+import { FaSearch } from "react-icons/fa";
 import LoadingSkeleton from "../ui/LoadingSkeleton";
 import { IoMdArrowRoundBack } from "react-icons/io";
 
@@ -19,6 +19,12 @@ function HotelsListPage() {
   const { data: { data: { data: hotels } = {} } = {}, isLoading } = useHotels({
     selectedStars,
   });
+
+  const handleSortChange = (selectedOption) => {
+    const sort = selectedOption.value;
+    searchParams.set("sortBy", sort);
+    setSearchParams(searchParams);
+  };
 
   const onSearchHandler = handleSubmit((data) => {
     if (!data?.search) {
@@ -70,36 +76,79 @@ function HotelsListPage() {
   }
 
   return (
-    <div>
-      <div className="z-10 h-24 overflow-hidden opacity-85">
-        {/* SEARCH */}
-        <div className="mx-auto w-[50vw] scale-150 rounded p-3">
-          <Search
-            onSearchHandler={onSearchHandler}
-            register={register}
-            isLoading={isLoading}
-            className="mt-4"
+    <div className="relative mt-1 flex min-h-screen w-full justify-center gap-4">
+      {/* filter/sort */}
+      <div className="sticky top-4 p-2">
+        <div className="flex flex-col items-center justify-center gap-6 space-y-8 p-2">
+          <StarRatingFilter
+            selectedStars={selectedStars}
+            onChange={handleStarsChange}
           />
         </div>
       </div>
 
-      <div className="flex w-full gap-4 p-6">
-        {/* filter/sort */}
-        <div className="sticky top-2 h-fit w-[25%] border-r-2 border-t-2 p-2">
-          <HotelListFilter isLoading={isLoading}>
-            <StarRatingFilter
-              selectedStars={selectedStars}
-              onChange={handleStarsChange}
-            />
-          </HotelListFilter>
+      <div className="-mt-6">
+        {/* SEARCH */}
+        <div className="flex w-full p-6">
+          <section className="flex flex-1 items-center justify-between gap-x-6 gap-y-4 rounded bg-black/5 p-6">
+            <div className="w-full">
+              <Select
+                onChange={handleSortChange}
+                options={[
+                  {
+                    label: "price per night (high first)",
+                    value: "pricePerNight-desc",
+                  },
+                  {
+                    label: "price per night (low first)",
+                    value: "pricePerNight-asc",
+                  },
+                  { label: "avgRating (high first)", value: "avgRating-desc" },
+                  {
+                    label: "low avgRating (low first)",
+                    value: "avgRating-asc",
+                  },
+                  { label: "recent first", value: "newest" },
+                  { label: "oldest first", value: "oldest" },
+                  { label: "a-z", value: "a-z" },
+                  { label: "z-a", value: "z-a" },
+                ]}
+                placeholder="sort hotels"
+              />
+            </div>
+
+            <form
+              className="group relative flex items-center justify-center"
+              onSubmit={onSearchHandler}
+            >
+              <div className="flex flex-row rounded bg-slate-200 shadow-lg">
+                <input
+                  type="search"
+                  disabled={isLoading}
+                  autoFocus
+                  className="rounded bg-inherit px-6 py-2 focus:outline-none disabled:cursor-not-allowed"
+                  placeholder="Search"
+                  {...register("search")}
+                />
+                <button
+                  type="submit"
+                  disabled={isLoading}
+                  className="-ml-12 rounded px-5 text-black/20 disabled:cursor-not-allowed"
+                >
+                  <FaSearch className="size-5" />
+                </button>
+              </div>
+            </form>
+          </section>
         </div>
-        {/* hotels list */}
-        <section className="w-[80%] rounded-md border-l-2 border-r-2 py-4 shadow-lg">
-          {/* hotel cards */}
-          {hotels.map((hotel, i) => (
-            <HotelsListItem hotel={hotel} key={i} />
-          ))}
-        </section>
+        <div className="flex w-full gap-4 px-6">
+          <section className="min-h-[100vh] rounded-md border-l-2 border-r-2 py-4 shadow-lg">
+            {/* hotel cards */}
+            {hotels.map((hotel, i) => (
+              <HotelsListItem hotel={hotel} key={i} />
+            ))}
+          </section>
+        </div>{" "}
       </div>
     </div>
   );
