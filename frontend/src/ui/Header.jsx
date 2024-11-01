285bc0d83d226dc0793660fa3d7d2a4a5bdb7bcf
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-11-01 15:41:14 +0300

    feat: update Header and Logo components | add arrow icon to Sign In link and update logo styling

--- frontend/src/ui/Header.jsx
+++ frontend/src/ui/Header.jsx
@@ -1,6 +1,7 @@
 import { Link, useLocation } from "react-router-dom";
 import HeaderAccount from "./HeaderAccount";
 import { useAuthContext } from "../context/AuthContext";
+import { FaArrowRight } from "react-icons/fa6";
 import Logo from "./Logo";
 import { cn } from "../utils/cn";
 
@@ -10,7 +11,7 @@ function Header() {
   console.log(location);
 
   return (
-    <section className="z-50 mx-auto mb-3 h-full w-full max-w-screen-xl border-b bg-slate-100 px-3 py-2 md:px-20">
+    <section className="z-50 mx-auto h-full w-full max-w-screen-xl border-b bg-slate-100 px-3 py-2 md:px-20">
       <header className="mx-auto flex h-[10vh] items-center justify-between px-6 py-1 opacity-85">
         <Logo />
         <nav>
@@ -51,9 +52,10 @@ function Header() {
               <li>
                 <Link
                   to="/login"
-                  className="rounded-lg bg-blue-600 px-5 py-2 text-white"
+                  className="flex items-center gap-2 rounded-lg bg-blue-600 px-5 py-2 text-white"
                 >
                   Sign In
+                  <FaArrowRight />
                 </Link>
               </li>
             )}
