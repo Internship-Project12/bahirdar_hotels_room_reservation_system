bee758b1ebe8533c0f22084b44d192f48e523965
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-11-01 09:37:16 +0300

    feat: configure email transport for production environment | set up SendGrid for email sending in production

--- backend/src/utils/email.js
+++ backend/src/utils/email.js
@@ -2,6 +2,16 @@ import nodemailer from 'nodemailer';
 
 // async..await is not allowed in global scope, must use a wrapper
 const sendEmail = async (options) => {
+  if (process.env.NODE_ENV === 'production') {
+    return nodemailer.createTransport({
+      service: 'SendGrid',
+      auth: {
+        user: process.env.SENDGRID_USERNAME,
+        pass: process.env.SENDGRID_PASSWORD,
+      },
+    });
+  }
+
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
