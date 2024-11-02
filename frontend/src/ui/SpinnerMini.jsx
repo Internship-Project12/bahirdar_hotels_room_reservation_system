defe9f04e9b557abc62756015973d9b189740cb9
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-11-02 21:39:25 +0300

    feat: enhance Spinner components with className prop for better customization | add className prop to Spinner and SpinnerMini components for improved styling flexibility

--- frontend/src/ui/SpinnerMini.jsx
+++ frontend/src/ui/SpinnerMini.jsx
@@ -1,8 +1,13 @@
+import { cn } from "../utils/cn";
+
 /* eslint-disable react/prop-types */
-function SpinnerMini({ color = "text-white" }) {
+function SpinnerMini({ color = "text-white", className = "" }) {
   return (
     <div
-      className={`text-surface inline-block h-4 w-4 animate-spin rounded-full border-2 border-solid border-current border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite] ${color}`}
+      className={cn(
+        `text-surface inline-block h-4 w-4 animate-spin rounded-full border-2 border-solid border-current border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite] ${color}`,
+        className,
+      )}
       role="status"
     >
       <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
@@ -12,4 +17,4 @@ function SpinnerMini({ color = "text-white" }) {
   );
 }
 
-export default SpinnerMini
+export default SpinnerMini;
