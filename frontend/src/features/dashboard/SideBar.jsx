91d6e173b3ec835f68cc05665e57c2448766981a
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-11-02 13:58:16 +0300

    feat: enhance SideBar component layout | improve styling and structure for better navigation experience

--- frontend/src/features/dashboard/SideBar.jsx
+++ frontend/src/features/dashboard/SideBar.jsx
@@ -4,22 +4,26 @@ import Logo from "../../ui/Logo";
 
 function SideBar({ menus }) {
   return (
-    <div className="flex min-h-screen w-[260px] flex-col gap-2 bg-gray-800 p-5 text-white">
-      <Logo />
+    <div className="flex min-h-screen w-[260px] flex-col gap-2 bg-gray-800 py-4 text-white">
+      <Logo className="text-white" />
 
       <hr className="border-b-2 border-gray-700" />
 
-      <nav className="mt-4 flex flex-col gap-3 p-4 text-lg uppercase transition">
-        {menus.map((menu) => (
-          <Link
-            key={menu.title}
-            className="flex items-center gap-3 rounded p-3 hover:bg-slate-700"
-            to={menu.url}
-          >
-            {menu.Icon}
-            <span>{menu.title}</span>
-          </Link>
-        ))}
+      <nav className="mt-4 uppercase transition">
+        <ul>
+          {menus.map((menu, i) => (
+            <li key={i}>
+              <Link
+                key={menu.title}
+                className="flex items-center gap-3 rounded-lg px-5 py-3 hover:bg-gray-900"
+                to={menu.url}
+              >
+                {menu.Icon}
+                <span>{menu.title}</span>
+              </Link>
+            </li>
+          ))}
+        </ul>
       </nav>
     </div>
   );
